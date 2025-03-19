39a107946e763a1b944ea744749149ce3d461663
Sebanti2003 <dasguptasebanti2003@gmail.com>
2025-03-19 23:09:53 +0530

    vercel app link

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ const io = new Server(server, {
       "http://localhost:5173",
       "http://localhost:5174",
       "https://citizens-advocatefrontend.vercel.app",
-      "https://citizensadvocatefrontend.onrender.com",
+ 
     ],
     methods: ["GET", "POST"],
     credentials: true,
@@ -63,7 +63,6 @@ app.set("trust proxy", 1);
 
 const allowedOrigins = [
   "https://citizens-advocatefrontend.vercel.app",
-  "https://citizensadvocatefrontend.onrender.com",
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5175",
